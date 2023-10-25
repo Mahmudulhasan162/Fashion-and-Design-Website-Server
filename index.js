de3c3600a77c10c41edf516e260ecd8de13b2908
Mahmudulhasan162 <mahmudulhasanshafi2345@gmail.com>
2023-10-25 21:33:38 +0600

    Delete Method applied. now user from front-end can delete their product and the product will be deleted from the cart database collection as well

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -328,6 +328,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
   })
+
+    app.delete("/cart/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: id}
+      const result = await cartProducts.deleteOne(query);
+      res.send(result);
+    })
 
 
     await client.db("admin").command({ ping: 1 });
